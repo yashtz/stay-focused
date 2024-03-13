9d4906a825aa29d58935f84bf2a9641483539d63
yashtz <118619651+yashtz@users.noreply.github.com>
2024-03-13 13:28:16 +0530

    Add files via upload

---
+++ stay-focused/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+import Home from './pages/Home';
+// import ToDoList from './pages/TodoList';
+// import Pomodoro from './pages/Pomodoro';
+// import ChatBot from './pages/ChatBot';
+// import TextToSpeech from './pages/TextToSpeech';
+// import BionicText from './pages/Bionic';
+// import Summariser from './pages/Summariser';
+
+
+function App() {  
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/todolist" element={<TodoList />} /> */}
+          {/* <Route path="/chatbot" element={<ChatBot />} /> */}
+          {/* <Route path="/pomodoro" element={<Pomodoro />} /> */}
+          {/* <Route path="/flashcards" element={<Flashcards />} /> */}
+          {/* <Route path="/summariser" element={<Summariser />} /> */}
+          {/* <Route path="/texttospeech" element={<TextToSpeech />} /> */}
+          {/* <Route path="/bionictext" element={<BionicText />} /> */}
+        </Routes>
+        {/* <Footer /> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ stay-focused/src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  // Define an array of page names
+  const pages = ['Home', 'TodoList', 'ChatBot', 'Pomodoro', 'Flashcards', 'Summariser', 'TextToSpeech', 'BionicText'];
+
+  return (
+    <nav className="bg-gradient-to-r font-ubuntu font-normal from-black via-gray-900 to-purple-700 p-4 flex justify-between items-center">
+      <div className="md:hidden">
+        <button
+          className="text-white focus:outline-none"
+          onClick={toggleMobileMenu}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="flex items-center justify-center">
+      <img src={require('../resources/mind.png')} alt="Mind" className="h-16" />
+      </div>
+      {isMobileMenuOpen && (
+        <div className="md:hidden fixed inset-0 bg-gradient-to-r from-black via-gray-900 to-purple-900 w-2/5 z-50">
+          <div className="flex justify-end p-4">
+            <button
+              className="text-white"
+              onClick={toggleMobileMenu}
+            >
+              Close
+            </button>
+          </div>
+          <div className="flex flex-col items-start p-4">
+            {/* Generate navigation links dynamically */}
+            {pages.map(page => (
+              <Link key={page} to={`/${page.toLowerCase()}`} className="text-white text-lg font-roboto">{page}</Link>
+            ))}
+          </div>
+        </div>
+      )}
+        <div className="hidden md:flex items-center justify-center flex-1 space-x-8">
+        {/* Generate navigation links dynamically */}
+        {pages.map(page => (
+                    <Link key={page} to={`/${page.toLowerCase()}`} className="text-white text-lg font-roboto hover:text-white hover:font-semibold hover:underline">
+                    <span className="glow-on-hover">{page}</span> {/* Added glow-on-hover class */}
+                  </Link>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
